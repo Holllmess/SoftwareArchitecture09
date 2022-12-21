6f3e9aa3e33357677031317cb8250ca514124c0e
Holllmess <spasskih221@gmail.com>
2022-12-21 13:27:18 +0300

    Add project files.

---
+++ WebApplication1/Models/WeatherForcast.cs
@@ -0,0 +1,33 @@
+﻿namespace WebApplication1.Models
+{
+    public class WeatherForecast
+    {
+
+        /*private DateTime _date;
+        public DateTime GetDate()
+        {
+            return _date;
+        }
+
+        public void SetDate(DateTime date)
+        {
+           _date = date;
+        }
+        */
+
+        public DateTime Date { get; set; }
+        public int TemperatureC { get; set; }
+        public int TemperatureF
+        {
+            get { return 32 + (int)(TemperatureC / 0.5556); }
+        }
+
+        public WeatherForecast(DateTime date, int temperatureC)
+        {
+            Date = date;
+            TemperatureC = temperatureC;   
+        }
+
+
+    }
+}
